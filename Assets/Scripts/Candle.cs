cf27e7b21f44a67f4f85f19d9990bf479a44774f
Kamran Charles Nayebi <dev.flipstudios@gmail.com>
2025-01-05 13:27:39 -0500

    Implemented new vfx for candles

--- Assets/Scripts/Candle.cs
+++ Assets/Scripts/Candle.cs
@@ -1,11 +1,18 @@
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.Events;
 
 public class Candle : MonoBehaviour
 {
     [SerializeField] private float waitTime = 20;
+    [SerializeField] private Object smokePrefab;
+    [SerializeField] private Object flamePrefab;
+    [SerializeField] private GameObject topPosition;
+    [SerializeField] private GameObject bottomPosition;
+    [SerializeField] private Light light;
     
     private float remainingTime;
+    private Object particleInstance;
     public float RemainingTime { get=>remainingTime; }
 
     private bool active = false;
@@ -26,9 +33,17 @@ public class Candle : MonoBehaviour
         {
             return;
         }
+
+        if (particleInstance != null)
+        {
+            Vector3 distance = topPosition.transform.position - bottomPosition.transform.position;
+            Vector3 position = distance * remainingTime / waitTime + bottomPosition.transform.position;
+            particleInstance.GetComponent<Transform>().position = position;
+            light.transform.position = position;
+        }
         if (!active)
         {
-            remainingTime += Time.deltaTime * 2;
+            remainingTime += Time.deltaTime * 5;
             
             if (meshRenderer != null)
             {
@@ -60,11 +75,23 @@ public class Candle : MonoBehaviour
     {
         active = true;
         remainingTime = waitTime;
+        if (particleInstance != null)
+            Destroy(particleInstance);
+        particleInstance = Instantiate(flamePrefab, topPosition.transform.position, topPosition.transform.rotation);
+        light.transform.position = topPosition.transform.position;
+        light.enabled = true;
     }
 
     public void StopTimer()
     {
         active = false;
+        if (particleInstance != null)
+            Destroy(particleInstance);
+        Vector3 distance = topPosition.transform.position - bottomPosition.transform.position;
+        Vector3 position = distance * remainingTime / waitTime + bottomPosition.transform.position;
+        particleInstance = Instantiate(smokePrefab, position, topPosition.transform.rotation);
+        light.transform.position = position;
+        light.enabled = false;
     }
 
 }
